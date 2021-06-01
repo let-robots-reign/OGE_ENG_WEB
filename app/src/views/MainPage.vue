fa7b555bf0786b249e99f17826d5929c66f5869f
Алексей <zotov.alexei.1@yandex.ru>
2021-06-01 22:04:43 +0300

    use vue3 setup() and onMounted()

--- app/src/views/MainPage.vue
+++ app/src/views/MainPage.vue
@@ -6,26 +6,29 @@
 import {API} from '@/api';
 import {OK_CODE} from '@/api/codes';
 import {useStore} from 'vuex';
+import {onMounted, ref} from 'vue';
 
 export default {
     name: 'MainPage',
-    data() {
-        return {
-            message: 'Not logged in'
-        };
-    },
-    beforeMount() {
+    setup() {
+        const message = ref('You are not logged in!');
         const store = useStore();
-        API.getCurrentUser()
-            .then(async (response) => {
-                if (response.status === OK_CODE) {
-                    this.message = `Hi, ${response.data.name}`;
-                    await store.dispatch('setAuth', true);
-                }
-            })
-            .catch(async () => {
-                await store.dispatch('setAuth', false);
-            });
+
+        onMounted(async () => {
+            API.getCurrentUser()
+                .then(async (response) => {
+                    if (response.status === OK_CODE) {
+                        message.value = `Hi, ${response.data.name}`;
+                        await store.dispatch('setAuth', true);
+                    }
+                })
+                .catch(async () => {
+                    await store.dispatch('setAuth', false);
+                });
+        });
+        return {
+            message
+        };
     }
 };
 </script>
