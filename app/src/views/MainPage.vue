cd4c131e593291807d0c138cc229fd16a2c95756
Алексей <zotov.alexei.1@yandex.ru>
2021-06-03 22:07:33 +0300

    main page markup

--- app/src/views/MainPage.vue
+++ app/src/views/MainPage.vue
@@ -1,6 +1,25 @@
 <template>
     <main class="main">
-        {{ message }}
+        <div class="main-page-sections">
+            <div class="main-page-section">
+                <p class="section-name">Тренировки</p>
+                <div class="trainings-grid">
+
+                </div>
+            </div>
+            <div class="main-page-section">
+                <p class="section-name">Варианты</p>
+                <div class="tests-carousel">
+
+                </div>
+            </div>
+            <div class="main-page-section">
+                <p class="section-name">Теория</p>
+                <div class="theory-grid">
+
+                </div>
+            </div>
+        </div>
     </main>
 </template>
 
@@ -11,6 +30,22 @@
     padding: 16px;
     color: var(--light-blue-shadow);
 }
+
+.main-page-sections {
+    width: 100%;
+    max-width: 500px;
+    margin: 0 auto;
+}
+
+.main-page-section {
+    margin: 32px;
+}
+
+.section-name {
+    font-size: 24px;
+    font-family: 'Comfortaa';
+}
+
 </style>
 
 <script>
