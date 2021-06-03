7d4885ac72605c4aba75e6b8439530348d0b9466
Алексей <zotov.alexei.1@yandex.ru>
2021-06-03 21:04:09 +0300

    use name in <router-link>s

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -6,14 +6,17 @@ import {PATHS} from '@/router/paths';
 
 const routes = [
     {
+        name: 'Main Page',
         path: PATHS.main,
         component: MainPage
     },
     {
+        name: 'Login',
         path: PATHS.login,
         component: Login
     },
     {
+        name: 'Signup',
         path: PATHS.signup,
         component: Signup
     }
