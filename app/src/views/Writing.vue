85f5f0c3cbf58801aac383af8fb02b10ef696486
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-08-24 20:49:40 +0400

    feature: show first writing task

--- app/src/views/Writing.vue
+++ app/src/views/Writing.vue
@@ -1,9 +1,9 @@
 <template>
     <main>
         <AppTrainingPage
-            topic="Письмо"
-            ref="page"
-            @check-answers="checkAnswers"
+                topic="Письмо"
+                ref="page"
+                @check-answers="checkAnswers"
         >
             <template #training-instruction>
                 <p>
@@ -27,16 +27,22 @@
                 <div class="card writing-task">
                     <div class="writing-task__section">
                         <p class="writing-task__title">Структура письма</p>
-                        <p>
+                        <p class="writing-task__hint">
                             Поставьте предложения в правильном порядке, чтобы получилось письмо.
                         </p>
-<!--                        <p v-for="(part, index) in task.letterParts" :key="index">-->
-<!--                            {{ part }}-->
-<!--                        </p>-->
+                        <p v-for="(part, index) in letterParts" :key="index">
+                            {{ index + 1 }}) {{ part }}
+                        </p>
                         <div class="letter-answers">
-                            <p><em>Впишите номера предложений в нужном порядке</em></p>
-<!--                            TODO: BaseInput v-for -->
+                            <BaseInput
+                                    v-for="index in letterParts.length"
+                                    :key="index"
+                                    :placeholder="index"
+                                    v-model="letterPartsAnswers[index - 1]"
+                                    class="letter-answers__input"
+                            />
                         </div>
+                        <p class="writing-task__hint">Впишите номера предложений в нужном порядке</p>
                     </div>
 
                     <div class="writing-task__section">
@@ -44,11 +50,11 @@
                         <p>
                             Расставьте слова по порядку.
                         </p>
-<!--                        <p v-for="(cliche, index) in task.cliches" :key="index">-->
-<!--                            {{ index + 1 }})-->
-<!--                            <span v-for="(word, index) in cliche" :key="index">{{ word }}</span>-->
-<!--&lt;!&ndash;                            TODO: BaseSelect v-for &ndash;&gt;-->
-<!--                        </p>-->
+            <!--                        <p v-for="(cliche, index) in task.cliches" :key="index">-->
+            <!--                            {{ index + 1 }})-->
+            <!--                            <span v-for="(word, index) in cliche" :key="index">{{ word }}</span>-->
+            <!--&lt;!&ndash;                            TODO: BaseSelect v-for &ndash;&gt;-->
+            <!--                        </p>-->
                     </div>
 
                     <div class="writing-task__section">
@@ -56,9 +62,9 @@
                         <p>
                             Совместите слова-связки с их русскими эквивалентами.
                         </p>
-<!--                        <p v-for="(linker, index) in task.linkers" :key="index">-->
-<!--                            {{ index + 1 }}) {{ linker }} - -->
-<!--                        </p>-->
+                        <!--                        <p v-for="(linker, index) in task.linkers" :key="index">-->
+                        <!--                            {{ index + 1 }}) {{ linker }} - -->
+                        <!--                        </p>-->
                         <p>
                             Дополните текст, используя слова-связки
                         </p>
@@ -68,19 +74,19 @@
                         <p class="writing-task__title">
                             Полные ответы
                         </p>
-<!--                        <div class="writing-task__full-answers"-->
-<!--                             v-for="(fullAnswersTask, index) in task.fullAnswers" :key="index">-->
-<!--                            <p>-->
-<!--                                Выберите лучший ответ на вопрос-->
-<!--                            </p>-->
-<!--                            <p>{{ fullAnswersTask.question }}</p>-->
-<!--&lt;!&ndash;                         TODO: add v-model &ndash;&gt;-->
-<!--                            <BaseRadioGroup-->
-<!--                                    name="fullAnswersRadio"-->
-<!--                                    :options="fullAnswersTask.options"-->
-<!--                                    vertical-->
-<!--                            />-->
-<!--                        </div>-->
+                <!--                        <div class="writing-task__full-answers"-->
+                <!--                             v-for="(fullAnswersTask, index) in task.fullAnswers" :key="index">-->
+                <!--                            <p>-->
+                <!--                                Выберите лучший ответ на вопрос-->
+                <!--                            </p>-->
+                <!--                            <p>{{ fullAnswersTask.question }}</p>-->
+                <!--&lt;!&ndash;                         TODO: add v-model &ndash;&gt;-->
+                <!--                            <BaseRadioGroup-->
+                <!--                                    name="fullAnswersRadio"-->
+                <!--                                    :options="fullAnswersTask.options"-->
+                <!--                                    vertical-->
+                <!--                            />-->
+                <!--                        </div>-->
                     </div>
                 </div>
             </template>
@@ -93,21 +99,26 @@
 import AppTrainingPage from '@/components/AppTrainingPage';
 import {onMounted, ref} from 'vue';
 import {API} from '@/services/api';
+import BaseInput from '@/components/form/BaseInput';
+
 export default {
     name: 'Writing',
-    components: {AppTrainingPage},
+    components: {BaseInput, AppTrainingPage},
     setup() {
         const page = ref([]);
+
         const letterParts = ref([]);
+        const letterPartsAnswers = ref([]);
+
         const userAnswers = ref([]);
 
         onMounted(async () => {
             API.getWritingTraining()
                 .then((result) => {
                     const task = result.data.task;
-
-                    letterParts.value = task.structure;
-
+                    console.log(task);
+                    letterParts.value = task.structure[0].task.split('\r\n');
+                    console.log(letterParts.value);
                 })
                 .catch((err) => console.log(err));
         });
@@ -119,6 +130,7 @@ export default {
         return {
             page,
             letterParts,
+            letterPartsAnswers,
             userAnswers,
             checkAnswers
         };
@@ -128,21 +140,33 @@ export default {
 
 <style lang="scss" scoped>
 main {
-    width: 50%;
-    margin: 32px auto;
+  width: 50%;
+  margin: 32px auto;
 }
 
 .writing-task {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    font-size: 18px;
-    padding: 1.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  font-size: 18px;
+  padding: 1.5rem;
 
-    &__title {
-         font-size: 1.7rem;
-         font-weight: 700;
-         margin-bottom: 12px;
-     }
+  &__title {
+    font-size: 1.7rem;
+    font-weight: 700;
+  }
+
+  &__hint {
+    margin: 24px 0;
+    font-style: italic;
+    text-align: center;
+  }
+}
+
+.letter-answers {
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  max-width: 70%;
+  margin: 16px auto;
 }
 </style>
