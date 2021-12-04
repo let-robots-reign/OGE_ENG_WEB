8f05255efdc884709c5141c7bc7a16436845ab05
let-robots-reign <zotov.alexei.1@yandex.ru>
2021-12-04 12:57:56 +0300

    login unit tests

---
+++ app/tests/unit/login.spec.js
@@ -0,0 +1,83 @@
+import { mount } from '@vue/test-utils';
+import Login from '@/views/Login';
+
+describe('Login.vue', () => {
+    it('renders two input fields', async () => {
+        const wrapper = mount(Login);
+        const emailLabel = wrapper.find('.form-control[type="email"] label');
+        const emailInput = wrapper.find('input[type="email"]');
+        const passwordLabel = wrapper.find('.form-control[type="password"] label');
+        const passwordInput = wrapper.find('input[type="password"]');
+
+        expect(emailLabel.text()).toBe('Email');
+        expect(passwordLabel.text()).toBe('Пароль');
+
+        const [email, pwd] = ['1@1.ru', 'qwerty'];
+
+        await emailInput.setValue(email);
+        await passwordInput.setValue(pwd);
+
+        expect(emailInput.element.value).toBe(email);
+        expect(passwordInput.element.value).toBe(pwd);
+    });
+
+    it('renders error for empty login', async () => {
+        const wrapper = mount(Login);
+        const emailInput = wrapper.find('input[type="email"]');
+        const passwordInput = wrapper.find('input[type="password"]');
+
+        const [email, pwd] = ['', 'qwerty'];
+
+        await emailInput.setValue(email);
+        await passwordInput.setValue(pwd);
+        await wrapper.find('button').trigger('click');
+
+        const loginErrorMessage = wrapper.find('.form-control[type="email"] .form-error-message');
+        expect(loginErrorMessage.text()).toBe('Поле не может быть пустым');
+    });
+
+    it('renders error for incorrect email', async () => {
+        const wrapper = mount(Login);
+        const emailInput = wrapper.find('input[type="email"]');
+        const passwordInput = wrapper.find('input[type="password"]');
+
+        const [email, pwd] = ['123', 'qwerty'];
+
+        await emailInput.setValue(email);
+        await passwordInput.setValue(pwd);
+        await wrapper.find('button').trigger('click');
+
+        const loginErrorMessage = wrapper.find('.form-control[type="email"] .form-error-message');
+        expect(loginErrorMessage.text()).toBe('Некорректный адрес электронной почты');
+    });
+
+    it('renders error for empty password', async () => {
+        const wrapper = mount(Login);
+        const emailInput = wrapper.find('input[type="email"]');
+        const passwordInput = wrapper.find('input[type="password"]');
+
+        const [email, pwd] = ['1@1.ru', ''];
+
+        await emailInput.setValue(email);
+        await passwordInput.setValue(pwd);
+        await wrapper.find('button').trigger('click');
+
+        const passwordErrorMessage = wrapper.find('.form-control[type="password"] .form-error-message');
+        expect(passwordErrorMessage.text()).toBe('Поле не может быть пустым');
+    });
+
+    it('renders error for short password', async () => {
+        const wrapper = mount(Login);
+        const emailInput = wrapper.find('input[type="email"]');
+        const passwordInput = wrapper.find('input[type="password"]');
+
+        const [email, pwd] = ['1@1.ru', '123'];
+
+        await emailInput.setValue(email);
+        await passwordInput.setValue(pwd);
+        await wrapper.find('button').trigger('click');
+
+        const passwordErrorMessage = wrapper.find('.form-control[type="password"] .form-error-message');
+        expect(passwordErrorMessage.text()).toBe('Пароль должен быть не короче 5 символов');
+    });
+});
