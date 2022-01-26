473985eb7b7d0ef210ccc5c814a8842acb81a571
let-robots-reign <zotov.alexei.1@yandex.ru>
2022-01-26 15:11:02 +0300

    create user controller

---
+++ server/controllers/user.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+
+class UserController {
+    async createUser(userData) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(userData.password, salt);
+
+        const user = new User({
+            name: userData.name,
+            email: userData.email,
+            desiredMark: userData.desiredMark,
+            password: hashedPassword
+        });
+
+        const result = await user.save();
+        // eslint-disable-next-line no-unused-vars
+        const {password, ...data} = await result.toJSON();
+        data.role = (process.env.ADMIN_LOGINS.split(',').includes(data.name)) ? 'admin' : 'user';
+        return data;
+    }
+
+    async checkIfExists(filter) {
+        return await User.exists(filter);
+    }
+
+    async getUser(filter) {
+        const user = await User.findOne(filter);
+        if (!user) return null;
+        const data = await user.toJSON();
+        data.role = (process.env.ADMIN_LOGINS.split(',').includes(data.name)) ? 'admin' : 'user';
+        return data;
+    }
+}
+
+module.exports = UserController;
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,36 +1,23 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const UserController = require('../controllers/user');
 
 require('dotenv').config();
 
+const userController = new UserController();
+
 router.post('/signup', async (req, res) => {
     const userData = req.body;
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(userData.password, salt);
-
-    const existing = await User.exists({email: userData.email});
+    // TODO: fields validation
+    const existing = await userController.checkIfExists({ email: userData.email });
     if (existing) {
         return res.status(400).send({
             message: 'user already exists',
         });
     }
-
-    const user = new User({
-        name: userData.name,
-        email: userData.email,
-        desiredMark: userData.desiredMark,
-        password: hashedPassword
-    });
-    // TODO: fields validation
-
-    const result = await user.save();
-    // eslint-disable-next-line no-unused-vars
-    const {password, ...data} = await result.toJSON();
-    data.role = (process.env.ADMIN_LOGINS.split(',').includes(data.name)) ? 'admin' : 'user';
-
-    const token = jwt.sign({_id: data._id}, process.env.JWT_SECRET);
+    const user = await userController.createUser(userData);
+    const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
 
     res.cookie('jwt', token, {
         httpOnly: true,
@@ -41,19 +28,19 @@ router.post('/signup', async (req, res) => {
 
     res.status(201).send({
         message: 'success',
-        user: data
+        user
     });
 });
 
 router.post('/login', async (req, res) => {
     const userData = req.body;
-    const user = await User.findOne({email: userData.email});
+    const user = await userController.getUser({ email: userData.email });
     if (!user) {
         return res.status(404).send({
             message: 'user not found'
         });
     }
-
+    console.log(user);
     if (!await bcrypt.compare(userData.password, user.password)) {
         return res.status(400).send({
             message: 'invalid credentials'
@@ -69,13 +56,11 @@ router.post('/login', async (req, res) => {
         secure: true,
     });
 
-    // eslint-disable-next-line no-unused-vars
-    const {password, ...data} = await user.toJSON();
-    data.role = (process.env.ADMIN_LOGINS.split(',').includes(data.name)) ? 'admin' : 'user';
+    delete user.password;
 
     res.status(200).send({
         message: 'success',
-        user: data
+        user
     });
 });
 
@@ -90,11 +75,9 @@ router.get('/user', async (req, res) => {
             });
         }
 
-        const user = await User.findOne({_id: token._id});
-        // eslint-disable-next-line no-unused-vars
-        const {password, ...data} = await user.toJSON();
-        data.role = (process.env.ADMIN_LOGINS.split(',').includes(data.name)) ? 'admin' : 'user';
-        res.status(200).send(data);
+        const user = await userController.getUser({ _id: token._id });
+        delete user.password;
+        res.status(200).send(user);
     } catch (e) {
         return res.status(401).send({
             message: `unauthenticated, ${e}`
